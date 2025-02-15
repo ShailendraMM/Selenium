1e28fffb71771826f941929ceec80a445bb05147
shailendra.mishra <shailendra.mishra@shailendramanimishra-l.vin.net>
2025-02-15 18:21:56 +0530

    commit2

--- src/test/java/solar/picking.java
+++ src/test/java/solar/picking.java
@@ -1,7 +1,10 @@
 package solar;
 
+import java.awt.Frame;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class picking {
@@ -13,7 +16,11 @@ public class picking {
 		driver.get("https://levissolaruat.vineretail.com/eRetailWeb/eRetailLogin.action?popup=true");
 		driver.findElement(By.xpath("//input[@id='userName']")).sendKeys("sa");
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("5d26259e554c415");
-		driver.findElement(By.xpath("//input[@value='Login']"));
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+        WebElement frame1=driver.findElement(By.xpath("iframe[@id='childIFrame']"))	;
+        
+        driver.switchTo().frame(frame1);
+        
 		
 
 	}
